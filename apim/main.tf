fed2b9f0f862e4d09fe8fe21076e1f887909bb7a
Fernando Cortés Hierro <fernandocorteshierro@gmail.com>
2023-05-23 10:10:32 +0200

    fixed TF storage account name

--- apim/main.tf
+++ apim/main.tf
@@ -17,7 +17,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "terraform-global-rg"
-    storage_account_name = "tfstategithub11184"
+    storage_account_name = "tfstategithubfecohi"
     key                  = "tfgithubactions.tfstate"
   }
 }
